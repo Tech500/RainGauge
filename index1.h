613656961ac3beb7c0c0cf2f85a2db1264a56603
William Lucid <11734452+Tech500@users.noreply.github.com>
2021-07-14 12:31:20 -0400

    7/14/2021

---
+++ index1.h
@@ -0,0 +1,46 @@
+//index1.h
+const char HTML1[] PROGMEM = R"====(
+<!DOCTYPE HTML>
+<html>
+
+<head>
+    <title>Weather Observations</title>
+</head>
+
+<body>
+    <h2>Rain Gauge<br>Indianapolis, Indiana</h2>
+    <br> LastUpdate: %LASTUPDATE%
+    <br> Latitude: %GPSLAT%
+    <br> Longitude : %GPSLNG%
+    <br> Elevation:  843 Feet.
+    <br> Temperature: %TEMP% F.
+    <br> Dewpoint:  %DEWPOINT% F.
+    <br> Heat Idex:  %HEATINDEX% F.
+    <br> Humidity: %HUM% %%
+    <br> Barometric Pressure: %SEALEVEL%  inHg.
+    <br> Difference:  %DIF% inHg --Past 15 Minutes
+    <br> Rain Day : %RAINDAY% Day/mm
+    <br> Rain Hour: %RAINHOUR% Hour/mm
+    <br> Rain 5 Min : %RAINFALL% Five Min/mm
+    <br>
+    <h2>Weather Observations</h2>
+    <h3>%DTSTAMP%</h3>
+    <br>
+    <a href=http://%LINK%/SdBrowse >File Browser</a>
+    <br>
+    <a href=http://%LINK%/Graphs>Graphed Weather Observations</a>
+    <br>
+    <a href=http://%LINK%/README.TXT >Readme</a>
+    <br>
+    <a href=http://%LINK%/SERVER.TXT >Server</a>
+    <br>
+    <a href=http://%LINK%/WIFI.TXT >WiFi</a>
+    <br>
+    <br>
+    <a href=https://forum.arduino.cc/index.php?topic=606947.0>Project Discussion</a>
+    <br>
+    <br> Client IP: %CLIENTIP%
+</body>
+
+</html>
+)====";
---
+++ variableInput.h
@@ -0,0 +1,67 @@
+//
+//   "RainGauge.ino" and  
+//   variableInput.h 
+//   William M. Lucid   07/012021 !16:00 EDT
+//
+// Replace with your network details  
+//const char * host  = "esp32";
+
+// Replace with your network details
+const char * ssid = "yourSSID";
+const char * password = "yourPASSWORD";
+
+//Settings pertain to NTP
+const int udpPort = 1337;
+//NTP Time Servers
+const char * udpAddress1 = "us.pool.ntp.org";
+const char * udpAddress2 = "time.nist.gov";
+
+//publicIP accessiable over Internet with Port Forwarding; know the risks!!!
+//WAN IP Address.  Or use LAN IP Address --same as server ip; no Internet access. 
+#define publicIP  "xx.xxx.xxx.xxx"  //Part of href link for "GET" requests
+String LISTEN_PORT = "8030"; //Part of href link for "GET" requests
+
+String linkAddress = "xx.xxx.xxx.xxx";  //publicIP and PORT for URL link
+
+String ip1String = "10.0.0.146";  //Host ip address  
+
+int PORT = 8030;  //Web Server port
+
+//Graphing requires "FREE" "ThingSpeak.com" account..  
+//Enter "ThingSpeak.com" data here....
+//Example data; enter yout account data..
+unsigned long int myChannelNumber = 123456; 
+const char * myWriteAPIKey = "E12345";
+
+//Server settings
+#define ip {10,0,0,100}
+#define subnet {255,255,255,0}
+#define gateway {10,0,0,1}
+#define dns {10,0,0,1}
+
+//webInterface --send Data to Domain, hosted web site
+const char * sendData = "DomainWebSite/collectdata2.php";
+
+//FTP Credentials
+const char * ftpUser = "user";
+const char * ftpPassword = "password";
+ 
+//Restricted Access
+const char* Restricted = "/ACCESS.TXT";  //Can be any filename.  
+//Will be used for "GET" request path to pull up client ip list.
+
+///////////////////////////////////////////////////////////
+//   "pulicIP/LISTEN_PORT/reset" wiill restart the server
+///////////////////////////////////////////////////////////
+
+///////////////// OTA Support //////////////////////////
+
+const char* http_username = "user";
+const char* http_password = "password";
+
+// xx.xx.xx.xx:yyyy/login will log in; this will allow updating firmware using:
+// xx.xx.xx.xx:yyyy/update
+//
+// xx.xx.xx.xx being publicIP and yyyy being PORT.
+//
+///////////////////////////////////////////////////////
