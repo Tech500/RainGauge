b909c67a94ab92f61a078b1e82fdd746c1323616
William Lucid <11734452+Tech500@users.noreply.github.com>
2021-07-15 14:52:58 -0400

    Add files via upload

---
+++ index2.h
@@ -0,0 +1,21 @@
+//index2.h
+const char HTML2[] PROGMEM = R"====(
+<!DOCTYPE HTML>
+<html>
+
+<head>
+    <title>SDBrowse</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+</head>
+
+<body>
+    <h2>File Reader
+    <br>
+    <br> %URLLINK% 
+    <br>
+    <br>  
+    <a href=http://%LINK%/Weather >Main Menu</a></h2><br>
+</body>
+
+</html>
+)====";
---
+++ index4.h
@@ -0,0 +1,20 @@
+//index4.h
+const char HTML4[] PROGMEM = R"====(
+<!DOCTYPE HTML>
+<html>
+
+<head>
+    <title>Restart</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+</head>
+
+<body>
+    <h2>Restarting Server</h2>
+    <br>
+    <br>
+    <br>
+    <a href=http://%LINK%/Weather >Main Menu</a>  
+</body>
+
+</html>
+)====";
